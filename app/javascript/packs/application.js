6ba764c7e44ed93ff637cc54b4f7be5abb53001e
Rodrigo Lima <rodrigo.cerqueira@gmail.com>
2021-11-05 16:27:06 -0300

    Included template DS GOV

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,3 +28,6 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+// Degign System import
+import "@govbr/dsgov/dist/dsgov.min.js";
