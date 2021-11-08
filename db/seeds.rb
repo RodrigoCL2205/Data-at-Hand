85aeccad452379616c071b8770f058650f5ed2b4
Fabio <fabioarrudamagalhaes@gmail.com>
2021-11-08 15:22:32 -0300

    seed Client model with cities and states

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+puts 'Cleaning clients database...'
+Client.destroy_all
+
+puts 'Starting clients seed...'
+
+path = Rails.root.join('db','estados-cidades.json')
+
+uf_cidades_url = File.read(path)
+estados_cidades = JSON.parse(uf_cidades_url)
+
+100.times do
+  estado = estados_cidades['estados'].sample
+  nome_estado = estado['nome']
+
+  cidade = estado['cidades'].sample
+
+  client = Client.create(
+  name: Faker::Company.name,
+  state: nome_estado,
+  city: cidade)
+  puts "Included #{client.name}"
+end
+
+puts "Clients insert succesfully"
