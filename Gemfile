1784af7d6817317906a276721c662cc46f11cbea
Ana Nascimento <anacdon@yahoo.com.br>
2021-11-08 17:02:38 -0300

    added action status to sample controller

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ gem 'redis', '~> 4.0'
 gem 'bootsnap', '>= 1.4.4', require: false
 
 gem 'devise'
-gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
+gem 'faker' #, :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 
 gem 'autoprefixer-rails', '10.2.5'
 gem 'font-awesome-sass'
@@ -63,4 +63,4 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-gem 'faker'
+# gem 'faker'
--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -6,7 +6,9 @@ class SamplesController < ApplicationController
     pacpoa
     pncp
     bebidas_nao_alcoolicas
-    @mic_outros = @mic.count - @pacpoa.sum - @listeria - @salmonela_frango - @salmonela_suino - @ecoli - @bebidas_nao_alcoolicas
+    @mic_outros = @mic.count - @pacpoa.sum - @listeria.count - @salmonela_frango - @salmonela_suino - @ecoli - @bebidas_nao_alcoolicas
+    @listeria_status = {}
+    @listeria_status = status(@listeria)
   end
 
   private
@@ -24,7 +26,7 @@ class SamplesController < ApplicationController
   end
 
   def pncp
-    @listeria = @mic.where("programa ILIKE ?", "%Listeria%").count
+    @listeria = @mic.where("programa ILIKE ?", "%Listeria%")
     @salmonela_frango = @mic.where("programa ILIKE ?", "%Aves%").count
     @ecoli = @mic.where("programa ILIKE ?", "%STEC%").count
     @salmonela_suino = @mic.where("programa ILIKE ?", "%Superfície%").count
@@ -32,5 +34,14 @@ class SamplesController < ApplicationController
 
   def bebidas_nao_alcoolicas
     @bebidas_nao_alcoolicas = @mic.where(matriz: "PRODUTOS DE ORIGEM VEGETAL").count
+  end
+
+  def status(variavel)
+    hash = {}
+    hash[:finalizada] = variavel.where(status: 'A', liberada: true).count
+    hash[:aguardando] = variavel.where(liberada: false).count
+    # hash[:rejeitada_interno] = variavel.where(status: 'R', liberada: true).count
+    # hash[:rejeitada_externo] = variavel.where(status: 'R', liberada: false).count
+    return hash
   end
 end
