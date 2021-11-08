9672e4fd76b6a5d9c52fdecc9466940acded0cde
Rodrigo Lima <rodrigo.cerqueira@gmail.com>
2021-11-08 13:56:47 -0300

    Seed of the sample model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+puts "Destoying all Samples."
+Sample.destroy_all
+puts "All samples destroyed."
+
+puts "parsing Samples CSV file."
+
+csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+filepath    = File.dirname(__FILE__) + "/2021114_11557_dado_bruto_form_049.csv"
+
+CSV.foreach(filepath, csv_options) do |row|
+  if Sample.where(sample_number: row['sample_number']) == []
+    puts "Creating sample."
+    sample = Sample.new(
+      sample_number: row['sample_number'],
+      client_id: 1,
+      data_recepcao: row['data_de_recepcao'],
+      programa: row['programa'],
+      matriz: row['matriz'],
+      subgrupo: row['subgrupo'],
+      rg: row['rg'],
+      area_analitica: row['area_analitica'],
+      status: row['status'],
+      objetivo_amostra: row['objetivo_da_amostra'],
+      liberada: row['liberada'] == 'T',
+      data_liberacao: row['data_da_liberacao'],
+      latente: row['latente'] == 'T',
+      descartada: row['descartada'],
+      data_descarte: row['data_do_descarte']
+    )
+    sample.save!
+    puts "Created sample #{sample.id}"
+  else
+    puts "Sample #{row['sample_number']} already exists"
+  end
+end
+
+puts "Parsing finished!"
