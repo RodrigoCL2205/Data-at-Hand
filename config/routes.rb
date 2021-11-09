6cc205ae240b2cf012736bb5972cb697b79c37ec
Ana Nascimento <anacdon@yahoo.com.br>
2021-11-09 16:23:27 -0300

    inserted query for time

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'indicadores/12', to: 'samples#twelve', as: :twelve
+  get 'indicadores', to: 'samples#data', as: :ask_time
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
