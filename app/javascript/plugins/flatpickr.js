2b4256d77f68843b283af58f83f52006666eed3a
Fabio <fabioarrudamagalhaes@gmail.com>
2021-11-11 17:48:57 -0300

    Ajustes no idioma e formatação do calendario

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -7,13 +7,19 @@
 // export { initFlatpickr };
 
 import flatpickr from "flatpickr";
+import { Portuguese } from "flatpickr/dist/l10n/pt.js"
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 const initFlatpickr = () => {
   flatpickr("#range_start", {
     altInput: true,
-    plugins: [new rangePlugin({ input: "#range_end" })]
+    plugins: [new rangePlugin({ input: "#range_end" })],
+    locale: Portuguese,
+    format: "d/m/Y",
+    altFormat: "d/m/Y",
+    dateFormat: 'd/m/Y'
   });
+
 }
 
 export { initFlatpickr };
