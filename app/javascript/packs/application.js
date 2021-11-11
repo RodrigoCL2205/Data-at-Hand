b732fe99b1f3dbea0f3f8697a2bcd4f30037ae7e
Rodrigo Lima <rodrigo.cerqueira@gmail.com>
2021-11-11 12:07:53 -0300

    Samples query

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,7 +31,7 @@ import { initFlatpickr } from "../plugins/flatpickr";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  initMenu();
+  // initMenu();
   initFlatpickr();
 });
 
